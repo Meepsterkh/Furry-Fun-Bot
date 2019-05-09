c85a8bf731ec989148aef7383f262c41c3b1a55d
Meepster Hooves <42324391+Meepsterkh@users.noreply.github.com>
2019-05-08 18:11:26 -0700

    Update MainBot.py

--- MainBot.py
+++ MainBot.py
@@ -9,6 +9,7 @@ import os
 
 # def main settings
 prefix = ("|")
+token = 'NTMwODc3MzUwMzM0OTU1NTIx.XNN5fA.gc5Setp3m182NmUO7flcdnTJAnY'
 
 # change main settings
 client = Bot(command_prefix=prefix)
@@ -30,11 +31,18 @@ class messageFun():
     def __init__(self, message):
         self.message = message
 
+    def url(self, strings : str, additive = None):
+        url = strings + additive
+        response = requests.get(url)
+        return(response.json())
+
+
+    async def sheriBot(self, yiffType):
+        value = self.url("https://sheri.bot/api/v2/", yiffType)["url"]
+        await client.send_message(self.message.channel, value)
 
     async def dadJoke(self):
-        url = "https://icanhazdadjoke.com/slack"
-        response = requests.get(url)
-        value = response.json()["attachments"][0]["text"]
+        value = self.url("https://icanhazdadjoke.com/slack")["attachments"][0]["text"]
         await client.send_message(self.message.channel, value)
 
 
@@ -44,6 +52,7 @@ async def on_member_join(member):
     roleNsfw = discord.utils.get(member.server.roles, name= 'Nsfw')
     await client.add_roles(member, roleMember)
     await client.add_roles(member, roleNsfw)
+    print("Added: " + str(member))
 
 
 @client.event
@@ -74,9 +83,12 @@ async def on_message(message):
         await client.delete_message(message)
         await client.send_message(message.channel, "Welcome to Mcdownald's \n Do you want a phucking \n Beese Churger?")
 
+        
     if message.content.startswith("|joke"):
         await extra.dadJoke()
-        
+
+    if message.content.startswith("|yiff"):
+        await extra.sheriBot("yiff")
 
 
 client.run(str(os.environ.get('BOT_TOKEN')))
